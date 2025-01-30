81b36e4987de969aeb73cef2aeb64b8a98621a0b
tridev222 <gangward82@gmail.com>
2025-01-30 21:38:28 +0530

    sum

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,sum=0;
+    cout <<"enter number a : ";
+    cin >> a;
+    cout <<"enter number b : ";
+    cin >> b;
+    sum = a+b;
+    cout << "sum = " << sum;
+    return 0;
+}
