021d0574cd109b16485675d31c05d6b3f1343459
tridev222 <gangward82@gmail.com>
2025-02-26 18:31:15 +0530

    leeet_53

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -1,27 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-
-using namespace std;
-
-int maxProduct(vector<int>& nums) {
-    int prefix = 1, suffix = 1, n = nums.size();
-    int max_product = INT_MIN;
-
-    for (int i = 0; i < n; i++) {
-        if (prefix == 0) prefix = 1;
-        if (suffix == 0) suffix = 1;
-
-        prefix *= nums[i];
-        suffix *= nums[n - i - 1];
-
-        max_product = max(max_product, max(prefix, suffix));
-    }
-    return max_product;
-}
-
-int main() {
-    vector<int> nums = {2, 3, -2, 4}; 
-    cout << "Maximum product subarray: " << maxProduct(nums) << endl;
-    return 0;
-}
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int sum = 0 , max_sum = INT_MIN;
+            for(int i = 0; i < nums.size(); i++){
+                sum += nums[i];
+                if(sum > max_sum){
+                    max_sum = sum;
+                }
+                if(sum < 0) sum = 0;
+            }
+            return max_sum;
+        }
+    };
