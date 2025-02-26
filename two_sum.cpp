42462e868005ba304ec3d4f9af008c04c59b33b3
tridev222 <gangward82@gmail.com>
2025-02-26 20:38:15 +0530

    leet_01

---
+++ two_sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, int> mpp;
+            for(int i = 0; i < nums.size(); i++){
+                int a = nums[i];
+                int more = target - a;
+                if(mpp.find(more) != mpp.end()){
+                    return {mpp[more], i};
+                }
+                mpp[a] = i;
+            }
+            return { -1, -1};
+        }
+    };
