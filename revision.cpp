590ec32e15d7b164a2edcdbcb9dc1f0b6799e558
devesh <deveh.gangwar2213@gmail.com>
2025-09-24 17:27:42 +0530

    practiciing again dsa started

---
+++ revision.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "enter number a" << endl;
+    cin >> a;
+    cout << "enter numberr b" << endl;
+    cin >> b;
+    cout << a + b << " is the sum of "<< a << " and " << b << endl;
+    cout << pow(a,10) << " is the tenth power of " << a << endl;
+    return 0;
+}
